a58a2511f7de8e3bdb750e3e94f515043c745eaa
jonpedigo <pedigojon@gmail.com>
2021-12-31 12:55:46 -0600

    updating

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,12 +4,15 @@ import {
     UNAUTH_USER,
     AUTH_ERROR
 } from './types';
+
 const ROOT_URL = process.env.API_URI || 'http://localhost:8000';
 
 axios.defaults.baseURL = ROOT_URL;
+
 if (localStorage.getItem('auth_jwt_token')) {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('auth_jwt_token');
 }
+
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export function signUserIn(data) {
